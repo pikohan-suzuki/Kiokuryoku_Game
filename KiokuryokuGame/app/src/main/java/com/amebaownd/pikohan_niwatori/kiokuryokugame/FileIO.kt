263ea9902ff14f42f97187fbe3a9f5cc8265d665
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-04-28 01:08:50 +0900

    内部ストレージのIO関数を追加

---
+++ KiokuryokuGame/app/src/main/java/com/amebaownd/pikohan_niwatori/kiokuryokugame/FileIO.kt
@@ -0,0 +1,19 @@
+package com.amebaownd.pikohan_niwatori.kiokuryokugame
+
+import android.app.Application
+import android.content.Context
+import java.io.BufferedReader
+import java.io.File
+
+fun readFile(context: Context, file:String): String?{
+    val readFile = File(context.filesDir,file)
+    if(readFile.exists()){
+        return readFile.bufferedReader().use(BufferedReader::readText)
+    }
+    return null
+}
+fun writeFile(context: Context,fileName:String,data:String){
+    File(context.filesDir,fileName).writer().use {
+        it.write(data)
+    }
+}
