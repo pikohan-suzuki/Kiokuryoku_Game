06053fb99629af85293b77529cfc884b0f7e879a
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-04-28 21:59:17 +0900

    ゲーム画面の処理を追加

--- KiokuryokuGame/app/src/main/java/com/amebaownd/pikohan_niwatori/kiokuryokugame/GameActivity.kt
+++ KiokuryokuGame/app/src/main/java/com/amebaownd/pikohan_niwatori/kiokuryokugame/GameActivity.kt
@@ -1,19 +1,96 @@
 package com.amebaownd.pikohan_niwatori.kiokuryokugame
 
 import android.os.Bundle
+import android.support.v4.app.FragmentManager
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_game.*
+import java.util.*
 
-class GameActivity : AppCompatActivity() {
+class GameActivity : AppCompatActivity(), ControllerFragment.OnButtonClickListener {
+    lateinit var sound: Sound
+    var continueFlg = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
 
-        val record =intent.getIntExtra("record",0)
+        val bestRecord = intent.getIntExtra("record", 0)
+
+        if (supportFragmentManager.findFragmentByTag("QuestionFragment") == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.question_frameLayout, newQuestionFragment(bestRecord), "QuestionFragment")
+                .commit()
+        }
+        if (supportFragmentManager.findFragmentByTag("ControllerFragment") == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.controller_frameLayout, newControllerFragment(),"ControllerFragment")
+                .commit()
+        }
+
+        sound = Sound(this)
+
+
+
+
+    }
+    override fun onResume() {
+        super.onResume()
+        sound.load()
+        val questionFragment = supportFragmentManager.findFragmentByTag("QuestionFragment") as QuestionFragment
+        var currentRecord = 0
+        while (continueFlg) {
+            val questionList: MutableList<Int> = mutableListOf(currentRecord + 1)
+            val rand = Random(Date().time)
+            for (i in 0..currentRecord) {
+                questionList[i] = rand.nextInt(4)
+            }
+            questionFragment.proposingQuestion(questionList,sound)
+            continueFlg = answer()
+            if (continueFlg)
+                currentRecord++
+//            else {
+//                endGame(currentRecord, )
+//            }
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        sound.unload()
+    }
+
+    override fun onButtonClicked(view: View) {
+        when (view.id) {
+            R.id.red_button ->
+                sound.play("dog")
+            R.id.blue_button ->
+                sound.play("bird")
+            R.id.green_button ->
+                sound.play("sheep")
+            R.id.orange_button ->
+                sound.play("cat")
+        }
+    }
+
+
+
+    private fun newRecord(record:Int) {
+        writeFile(this,"bestRecord",record.toString())
+    }
+
+    private fun question(questionList: MutableList<Int>) {
 
     }
 
-    private fun newRecord(){
+    private fun answer(): Boolean {
 
+        return false
     }
+
+//    private fun endGame(record: Int, bestRecord: Int) {
+//        if (record > bestRecord)
+//            newRecord(record)
+//    }
 }
