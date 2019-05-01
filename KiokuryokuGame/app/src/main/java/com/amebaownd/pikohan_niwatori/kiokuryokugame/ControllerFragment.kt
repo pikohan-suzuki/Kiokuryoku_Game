4a4921cabb8529e775ed8ea32bfd5aab429d7393
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-05-02 02:08:14 +0900

    imageリソースの変更

--- KiokuryokuGame/app/src/main/java/com/amebaownd/pikohan_niwatori/kiokuryokugame/ControllerFragment.kt
+++ KiokuryokuGame/app/src/main/java/com/amebaownd/pikohan_niwatori/kiokuryokugame/ControllerFragment.kt
@@ -1,12 +1,10 @@
 package com.amebaownd.pikohan_niwatori.kiokuryokugame
 
-import android.app.Application
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageView
 
 
@@ -21,16 +19,16 @@ class ControllerFragment() : Fragment() {
     lateinit var orange_Button :ImageView
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_controller, container, false)
-        blue_Button=view.findViewById<ImageView>(R.id.blue_button)
+        blue_Button=view.findViewById<ImageView>(R.id.cat_image)
         blue_Button.setOnClickListener{ val listener = context as? OnButtonClickListener
             listener?.onButtonClicked(it) }
-        red_Button=view.findViewById<ImageView>(R.id.green_button)
+        red_Button=view.findViewById<ImageView>(R.id.bird_image)
         red_Button.setOnClickListener{ val listener = context as? OnButtonClickListener
             listener?.onButtonClicked(it) }
-        green_Button=view.findViewById<ImageView>(R.id.red_button)
+        green_Button=view.findViewById<ImageView>(R.id.dog_image)
         green_Button.setOnClickListener{ val listener = context as? OnButtonClickListener
             listener?.onButtonClicked(it) }
-        orange_Button=view.findViewById<ImageView>(R.id.orange_button)
+        orange_Button=view.findViewById<ImageView>(R.id.sheep_image)
         orange_Button.setOnClickListener{ val listener = context as? OnButtonClickListener
             listener?.onButtonClicked(it) }
         return view
