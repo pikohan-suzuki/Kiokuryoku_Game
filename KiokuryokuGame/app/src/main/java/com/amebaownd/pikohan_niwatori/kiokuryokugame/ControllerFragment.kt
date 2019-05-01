28008ee8ff7499202d98679a8c06174fb31c5e50
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-05-01 14:06:59 +0900

    clickableを変更する処理を追加

--- KiokuryokuGame/app/src/main/java/com/amebaownd/pikohan_niwatori/kiokuryokugame/ControllerFragment.kt
+++ KiokuryokuGame/app/src/main/java/com/amebaownd/pikohan_niwatori/kiokuryokugame/ControllerFragment.kt
@@ -15,17 +15,38 @@ class ControllerFragment() : Fragment() {
         fun onButtonClicked(view: View)
     }
 
+    lateinit var blue_Button :ImageView
+    lateinit var red_Button :ImageView
+    lateinit var green_Button :ImageView
+    lateinit var orange_Button :ImageView
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_controller, container, false)
-        view.findViewById<ImageView>(R.id.blue_button).setOnClickListener{ val listener = context as? OnButtonClickListener
+        blue_Button=view.findViewById<ImageView>(R.id.blue_button)
+        blue_Button.setOnClickListener{ val listener = context as? OnButtonClickListener
             listener?.onButtonClicked(it) }
-        view.findViewById<ImageView>(R.id.green_button).setOnClickListener{ val listener = context as? OnButtonClickListener
+        red_Button=view.findViewById<ImageView>(R.id.green_button)
+        red_Button.setOnClickListener{ val listener = context as? OnButtonClickListener
             listener?.onButtonClicked(it) }
-        view.findViewById<ImageView>(R.id.red_button).setOnClickListener{ val listener = context as? OnButtonClickListener
+        green_Button=view.findViewById<ImageView>(R.id.red_button)
+        green_Button.setOnClickListener{ val listener = context as? OnButtonClickListener
             listener?.onButtonClicked(it) }
-        view.findViewById<ImageView>(R.id.orange_button).setOnClickListener{ val listener = context as? OnButtonClickListener
+        orange_Button=view.findViewById<ImageView>(R.id.orange_button)
+        orange_Button.setOnClickListener{ val listener = context as? OnButtonClickListener
             listener?.onButtonClicked(it) }
         return view
+    }
+
+    fun setUnclickaable(){
+        blue_Button.isClickable=false
+        red_Button.isClickable=false
+        green_Button.isClickable=false
+        orange_Button.isClickable=false
+    }
+    fun setClickable(){
+        blue_Button.isClickable=true
+        red_Button.isClickable=true
+        green_Button.isClickable=true
+        orange_Button.isClickable=true
     }
 }
 
