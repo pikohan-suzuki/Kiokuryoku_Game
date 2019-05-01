22b805a57d2ea32cc3b4bc154e9019a1d542043c
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-05-01 14:03:38 +0900

    newrecordの表示をダイヤログに変更

---
+++ KiokuryokuGame/app/src/main/java/com/amebaownd/pikohan_niwatori/kiokuryokugame/NewRecordDialog.kt
@@ -0,0 +1,33 @@
+package com.amebaownd.pikohan_niwatori.kiokuryokugame
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.app.Dialog
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.view.View
+import android.widget.Button
+
+class NewRecordDialog:DialogFragment(){
+    lateinit var ctx : Context
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val alertView: View = activity!!.layoutInflater.inflate(R.layout.fragment_information,null)
+        val okButton = alertView.findViewById<Button>(R.id.information_ok)
+        okButton.setOnClickListener{
+            val result= Intent()
+            val pendingIntent =activity!!.createPendingResult(targetRequestCode,result, PendingIntent.FLAG_ONE_SHOT)
+            try{
+                pendingIntent.send(Activity.RESULT_OK)
+            }catch (ex: PendingIntent.CanceledException){
+                ex.printStackTrace()
+            }
+            dialog.dismiss()
+        }
+        val dialogBuilder = AlertDialog.Builder(activity)
+        dialogBuilder.setView(alertView)
+        return dialogBuilder.create()
+    }
+}
