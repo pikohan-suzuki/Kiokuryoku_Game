bf0cc3b5c0a2ca34b98002a4b41ac7e3526b8e5e
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-05-01 13:57:41 +0900

    ダイヤログ管理用のクラスを追加

---
+++ KiokuryokuGame/app/src/main/java/com/amebaownd/pikohan_niwatori/kiokuryokugame/DialogManager.kt
@@ -0,0 +1,46 @@
+package com.amebaownd.pikohan_niwatori.kiokuryokugame
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.app.Dialog
+import android.app.PendingIntent
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.support.v4.app.FragmentManager
+import android.view.View
+import android.widget.Button
+import java.lang.Thread.sleep
+
+class MakeTestDialog(context: Context,private val value:Int=0 ){
+    fun showDialog(requestCode: Int, manager: FragmentManager) {
+        when (requestCode) {
+            201 -> {
+                val dialog: InformationDialog = InformationDialog()
+                dialog.setTargetFragment(null, requestCode)
+                dialog.show(manager, "informationDialog")
+            }
+            202 -> {
+                val dialog: CountPopupDialog = CountPopupDialog()
+                dialog.count=value
+                dialog.setTargetFragment(null, requestCode)
+                dialog.show(manager, "countPopupDialog")
+//                sleep(1000)
+//                dialog.dismiss()
+            }
+            203 -> {
+                val dialog: NewRecordDialog = NewRecordDialog()
+                dialog.setTargetFragment(null, requestCode)
+                dialog.show(manager, "newRecordDialog")
+            }
+            204 -> {
+                val dialog: ContinueMenuDialog = ContinueMenuDialog()
+                dialog.record =value
+                dialog.setTargetFragment(null, requestCode)
+                dialog.show(manager, "continueMenuDialog")
+            }
+        }
+    }
+}
